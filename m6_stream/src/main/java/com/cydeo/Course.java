0576821b2622c9f41d6747568b660a80724744ff
marika <tsotsonavamarika@gmail.com>
2022-08-18 23:10:51 -0400

    update, new subj

---
+++ m6_stream/src/main/java/com/cydeo/Course.java
@@ -0,0 +1,18 @@
+package com.cydeo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+public class Course {
+
+    private String courseName;
+    private int courseId;
+
+}
---
+++ m6_stream/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,33 @@
+package com.cydeo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+       //Creating Stream from Array
+        String[] courses = {"Java", "Spring", "Agile"};
+
+        Stream<String> courseStream = Arrays.stream(courses);
+
+        //Creating stream from collection
+        List<String> courseList = Arrays.asList("Java", "Spring", "Agile");
+        Stream<String> courseStream2 = courseList.stream();
+
+        List<Course> myCourse = Arrays.asList(
+                new Course("Java",100),
+                new Course("DS",101),
+                new Course("MS",102)
+        );
+        Stream<Course> myCourseStream = myCourse.stream();
+
+
+
+
+
+
+
+    }
+}
