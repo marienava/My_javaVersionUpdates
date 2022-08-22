b998de0576a8c532406dcb06bbea05623115b753
marika <tsotsonavamarika@gmail.com>
2022-08-22 15:04:42 -0400

    BigDecimal/Optional demo

--- m8_newVersionUpdates/src/main/java/com/cydeo/java9/FactoryMethodsDemo.java
+++ m8_newVersionUpdates/src/main/java/com/cydeo/java9/FactoryMethodsDemo.java
@@ -1,4 +1,35 @@
 package com.cydeo.java9;
 
+import java.util.*;
+
 public class FactoryMethodsDemo {
+    public static void main(String[] args) {
+
+        /*
+        //Creating unmodifiable List before java9
+        List<String> courses = Collections.unmodifiableList(Arrays.asList("Java", "Spring", "Agile"));
+        courses.add("TS");
+        System.out.println(courses);
+
+         */
+
+
+        //Creating unmodifiable List after java9
+        List<String> myCourse = List.of("Java", "Spring", "Agile");
+        System.out.println(myCourse);
+
+        //Creating unmodifiable Set after java9
+        Set<String> myProducts = Set.of("Milk", "Bread", "Butter");
+        System.out.println(myProducts);
+
+        //Creating unmodifiable Map after java9
+        Map<String, Integer> myCart = Map.ofEntries(
+                Map.entry("Samsung TV", 1),
+                Map.entry("PSP", 1),
+                Map.entry("Chair", 5)
+        );
+        System.out.println(myCart);
+
+    }
+
 }
