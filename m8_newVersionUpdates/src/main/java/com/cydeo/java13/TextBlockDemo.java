e8a435d7a0d44448ba90ce8f4964c309c484ee9f
marika <tsotsonavamarika@gmail.com>
2022-08-23 23:34:09 -0400

    java

---
+++ m8_newVersionUpdates/src/main/java/com/cydeo/java13/TextBlockDemo.java
@@ -0,0 +1,21 @@
+package com.cydeo.java13;
+
+public class TextBlockDemo {
+    public static void main(String[] args) {
+
+        var address = "7925 Jones Branch Drive \n " +
+                "McLean, Va 22102";
+        System.out.println(address);
+
+        var address2 = """
+                7925 Jones Branch Dr
+                McLean, Va 22102
+                """;
+        System.out.println(address2);
+
+
+
+
+
+    }
+}
---
+++ m8_newVersionUpdates/src/main/java/com/cydeo/java14/SwitchCaseDemo.java
@@ -0,0 +1,49 @@
+package com.cydeo.java14;
+
+public class SwitchCaseDemo {
+    public static void main(String[] args) {
+
+        var month = 4;
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("this month has 31 days");
+                break;
+            case 4:
+            case 6:
+            case 9:
+                System.out.println("this month has 30 days");
+                break;
+            case 2:
+                System.out.println("February can have 28 or 29 days");
+                break;
+            default:
+                System.out.println("invalid month");
+        }
+
+        System.out.println("======================");
+        switch(month) {
+            case 1,3,5,7,8,10,12 -> System.out.println("This month has 31 days");
+            case 4,6,9 -> System.out.println("This month has 30 days");
+            case 2 -> System.out.println("This month has 28 days or 29 days");
+            default -> System.out.println("Invalid month");
+        }
+
+        System.out.println("======================");
+
+        int day1 = switch(month){
+            case 1,3,5,7,8,10,12 -> 31;
+            case 4,6,9 -> 30;
+            case 2 -> 29;
+            default -> 0;
+        };
+        System.out.println(day1);
+
+
+    }
+}
