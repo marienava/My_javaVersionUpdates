f195f2928458b9571b5963c8fe2c11c64d392bbe
marika <tsotsonavamarika@gmail.com>
2022-08-25 23:37:43 -0400

    java 14

---
+++ m8_newVersionUpdates/src/main/java/com/cydeo/java14/User.java
@@ -0,0 +1,14 @@
+package com.cydeo.java14;
+
+public record User(String name, String address) {
+
+    // ALL ARG CONST
+    //GETTER
+    //TO STRING
+    //EQUALS
+    //HASH CODE
+
+    // each filled is private final, it doesn't have setter
+
+
+}
